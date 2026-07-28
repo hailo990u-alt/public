99541075e27975f6d935f6e3bb4f43ffb46cbb22
hailo990u-alt <hailo990u@gmail.com>
2026-07-29 01:48:11 +0300

    Upgrade KROZO premium storefront design

--- assets/krozo.js
+++ assets/krozo.js
@@ -1,1 +1,16 @@
-document.addEventListener('click',e=>{if(e.target.closest('[data-menu-toggle]')){const n=document.querySelector('[data-mobile-nav]');n.hidden=!n.hidden}});
+document.addEventListener('click',e=>{
+  if(e.target.closest('[data-menu-toggle]')){
+    const n=document.querySelector('[data-mobile-nav]');
+    n.hidden=!n.hidden;
+  }
+  if(e.target.closest('[data-sticky-submit]')){
+    document.querySelector('.product-atc')?.click();
+  }
+});
+
+const atc=document.querySelector('.product-atc');
+const sticky=document.querySelector('[data-sticky-atc]');
+if(atc&&sticky){
+  const observer=new IntersectionObserver(([entry])=>{sticky.hidden=entry.isIntersecting},{threshold:.1});
+  observer.observe(atc);
+}
